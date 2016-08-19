c5192396c69b9c95d9f78b80601b90319da34512
chren <peter.privalinec@gmail.com>
2016-08-19 14:57:49 +0200

    sticky navigation menu

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,27 @@
+$(document).ready(function() {
+    var stickyToggle = function(sticky, stickyWrapper, scrollElement) {
+        var stickyHeight = sticky.outerHeight();
+        var stickyTop = stickyWrapper.offset().top;
+        if (scrollElement.scrollTop() >= stickyTop){
+            stickyWrapper.height(stickyHeight);
+            sticky.addClass("is-sticky");
+        }
+        else{
+            sticky.removeClass("is-sticky");
+            stickyWrapper.height('auto');
+        }
+    };
 
+    $('[data-toggle="sticky-onscroll"]').each(function() {
+        var sticky = $(this);
+        var stickyWrapper = $('<div>').addClass('sticky-wrapper');
+        sticky.before(stickyWrapper);
+        sticky.addClass('sticky');
+
+        $(window).on('scroll.sticky-onscroll resize.sticky-onscroll', function() {
+            stickyToggle(sticky, stickyWrapper, $(this));
+        });
+
+        stickyToggle(sticky, stickyWrapper, $(window));
+    });
+});
