4edf2d2168d779cd0223e27b62e006d8b7f2d4d0
chren <peter.privalinec@gmail.com>
2016-09-09 14:16:10 +0200

    updates in features section

--- static/js/features.js
+++ static/js/features.js
@@ -18,17 +18,17 @@ $(document).ready(function () {
                 'headline' : featuresList.getElementsByTagName('h2')[0],
                 'coor_text': {
                     x: 20,
-                    y: 20
+                    y: 80
                 },
                 'coor_target' : {
-                    x: center.x,
-                    y: center.y - 50
+                    x: center.x + 5,
+                    y: center.y - 43
                 }
             },
             'case' : {
                 'coor_text': {
                     x: width - 20,
-                    y: 20
+                    y: 80
                 },
                 'coor_target' : {
                     x: center.x + 5,
@@ -41,8 +41,8 @@ $(document).ready(function () {
                     y: center.y - 20
                 },
                 'coor_target' : {
-                    x: center.x,
-                    y: center.y + 25
+                    x: center.x - 20,
+                    y: center.y + 23
                 }
             },
             'inside' : {
@@ -51,8 +51,8 @@ $(document).ready(function () {
                     y: center.y + 20
                 },
                 'coor_target' : {
-                    x: center.x - 5,
-                    y: center.y + 125
+                    x: center.x + 25,
+                    y: center.y + 95
                 }
             },
             'usb'  : {
@@ -61,7 +61,7 @@ $(document).ready(function () {
                     y: height - 120
                 },
                 'coor_target' : {
-                    x: center.x,
+                    x: center.x + 10,
                     y: center.y + 185
                 }
             }
@@ -71,16 +71,50 @@ $(document).ready(function () {
     function paintEntry(key) {
         ctx.beginPath();
         ctx.lineWidth = 2;
-        ctx.strokeStyle = '#fafafa';
-        var x = feature[key].coor_text.x;
-        var y = feature[key].coor_text.y;
-        ctx.moveTo(x , y);
-        x = feature[key].coor_target.x;
-        y = feature[key].coor_target.y;
-        ctx.lineTo(x, y);
-        ctx.closePath();
+        ctx.strokeStyle = '#ddd';
+
+        // obtain source target coordinates
+        var trgtX = feature[key].coor_target.x;
+        var trgtY = feature[key].coor_target.y;
+
+        // obtain text block coordinates
+        var txtX = feature[key].coor_text.x;
+        var txtY = feature[key].coor_text.y;
+
+        // paint source target circle
+        ctx.arc(txtX < trgtX ? trgtX + 6 : trgtX - 6,trgtY,6,0,2*Math.PI);
+
+        // calculate difference step
+        var temp = trgtY - txtY;
+        ctx.moveTo(trgtX , trgtY);
+        if(txtX < trgtX) {
+            // text on left
+            var newX = trgtX - 120;
+            ctx.lineTo(newX, trgtY);
+            ctx.lineTo(newX - temp, txtY);
+
+        } else {
+            // text on right
+            newX = trgtX + 120;
+            ctx.lineTo(newX, trgtY);
+            ctx.lineTo(newX + temp, txtY);
+        }
+
+        // finishing line to text
+        ctx.lineTo(txtX, txtY);
         ctx.stroke();
-        //console.log(key, feature[key]);
+
+        // draw text - headline
+        var textBlock = document.getElementById(key);
+        var title = textBlock.getElementsByTagName('h3')[0].innerHTML;
+
+        ctx.fillStyle = '#ddd';
+        ctx.font="23px 'Open Sans', sans-serif";
+        var titleWidth = ctx.measureText(title).width;
+        ctx.fillText(title, txtX < trgtX ? txtX : txtX - titleWidth, txtY - 12);
+
+        // var text =  textBlock.getElementsByTagName('p')[0].innerHTML;
+        // console.log(title, text);
     }
 
     function draw() {
