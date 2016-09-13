92b044a2cd678ac5825782546a3ce6fc9e83b782
chren <peter.privalinec@gmail.com>
2016-09-13 17:18:13 +0200

    some minor changes in spacing, etc ...

--- static/js/team.js
+++ static/js/team.js
@@ -1,28 +1,27 @@
+(function ($) {
 
-(function($){
+  $.fn.shuffle = function () {
 
-    $.fn.shuffle = function() {
+    var allElems = this.get(),
+      getRandom = function (max) {
+        return Math.floor(Math.random() * max);
+      },
+      shuffled = $.map(allElems, function () {
+        var random = getRandom(allElems.length),
+          randEl = $(allElems[ random ]).clone(true)[ 0 ];
+        allElems.splice(random, 1);
+        return randEl;
+      });
 
-        var allElems = this.get(),
-            getRandom = function(max) {
-                return Math.floor(Math.random() * max);
-            },
-            shuffled = $.map(allElems, function(){
-                var random = getRandom(allElems.length),
-                    randEl = $(allElems[random]).clone(true)[0];
-                allElems.splice(random, 1);
-                return randEl;
-            });
+    this.each(function (i) {
+      $(this).replaceWith($(shuffled[ i ]));
+    });
 
-        this.each(function(i){
-            $(this).replaceWith($(shuffled[i]));
-        });
+    return $(shuffled);
 
-        return $(shuffled);
-
-    };
+  };
 
 })(jQuery);
 $(document).ready(function () {
-    $('.person').shuffle();
+  $('.person').shuffle();
 });
