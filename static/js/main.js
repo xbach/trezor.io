2c7a029cc72b29b49fea2256de029fc10d937bb8
chren <peter.privalinec@gmail.com>
2017-01-06 19:14:56 +0100

    added responsive bgs, sticky navigation and others

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,9 @@
 /* our code */
 $(document).ready(function () {
 
-  var OSName="unknown";
+  var OSName="unknown",
+      pos = $('#usage').offset().top,
+      doc = $(document);
   if (navigator.appVersion.indexOf("Win")!=-1) OSName="win";
   if (navigator.appVersion.indexOf("Mac")!=-1) OSName="mac";
   if (navigator.appVersion.indexOf("X11")!=-1) OSName="linux";
@@ -12,6 +14,9 @@ $(document).ready(function () {
   $("#headline").fitText(1.1);
   $("#lead").fitText(1.8);
 
+  // jumbo height hotfix
+  // $('#jumbotron').css('height', $(window).height() + 'px');
+
   $('#lead-typer').typed({
     stringsElement: $('#lead-ghost'),
     backDelay: 500,
@@ -19,5 +24,18 @@ $(document).ready(function () {
     loop: true
   });
 
+  function createStickyNav(sticky) {
+    if (typeof sticky !== 'undefined') {
+      doc.on('scroll', function() {
+        checkPosition(sticky);
+      });
+    }
+  }
 
+  function checkPosition(sticky) {
+    doc.scrollTop() >= pos ? sticky.addClass("is-sticky") : sticky.removeClass("is-sticky");
+  }
+
+  createStickyNav($("#sticky-nav"));
 });
+
