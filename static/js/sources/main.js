bfcbb898f03aeceda4aa1e348e23a8bbf0849313
chren <peter.privalinec@gmail.com>
2017-01-12 18:28:16 +0100

    added lazyloading

--- static/js/sources/main.js
+++ static/js/sources/main.js
@@ -136,6 +136,16 @@ $(document).ready(function () {
   });
 
   handleAffil();
+
+  var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+    webkitRequestAnimationFrame || msRequestAnimationFrame;
+  if (raf) {
+    raf(function() {
+      $("img.lazy").lazyload({
+        threshold : 500
+      }); })
+  };
+
   shuffleTeam($('.shuffle'));
   createStickyNav($("#sticky-nav"));
 
