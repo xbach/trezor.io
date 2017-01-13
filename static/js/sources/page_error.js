9927c4b504f9fdabb18691b7b4c430b020dba3aa
chren <peter.privalinec@gmail.com>
2017-01-13 17:47:21 +0100

    replaced js stuff out of view

---
+++ static/js/sources/page_error.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+  // Add the missing url to the text
+  $("#url").text(window.location.pathname);
+
+  var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+    webkitRequestAnimationFrame || msRequestAnimationFrame;
+  if (raf) {
+    raf(function() {
+      $("img.lazy").lazyload({
+        threshold : 500
+      });
+    });
+  }
+
+  // Fix the css locations dynamically
+  $("head link").each(function() {
+    for (var i=0; i<window.location.pathname.split(/\//g).length-2; i++)
+      $(this).attr("href", "../" + $(this).attr("href"));
+  });
+})
