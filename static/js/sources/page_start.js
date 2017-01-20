f8592fd50d68ef0267af537767250d98afe7ab03
chren <peter.privalinec@gmail.com>
2017-01-20 18:00:57 +0100

    /start page

--- static/js/sources/page_start.js
+++ static/js/sources/page_start.js
@@ -11,6 +11,55 @@ $(document).ready(function () {
   }
 
   var $start = $('#start'),
-    h = window.innerHeight - 280;
+    h = window.innerHeight - 280,
+    index = 0;
   $start.css('min-height', h + 'px');
+
+
+  var $form = $('#mail-subscribe');
+
+
+  if ( $form.length > 0 ) {
+    $('#mail-subscribe input[type="submit"]').bind('click', function ( event ) {
+      if ( event ) event.preventDefault();
+      var mail = $('#mail-subscribe input[type="email"]')[0].value;
+      if ( isEmail(mail) ) {
+        register();
+      } else {
+        $('#mail-subscribe input[type="email"]').addClass('red');
+      }
+    });
+  }
+
+  function gotoNext() {
+    $("#step" + index).fadeOut('fast', function() {
+      index++;
+      $("#step" + index).fadeIn('fast');
+    })
+  }
+
+  function isEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+  }
+
+  function register() {
+    console.warn('register');
+    $.ajax({
+      type: $form.attr('method'),
+      url: $form.attr('action'),
+      data: $form.serialize(),
+      cache       : false,
+      dataType    : 'json',
+      contentType: "application/json; charset=utf-8",
+      error       : function(err) { alert("Could not connect to the registration server. Please try again later."); },
+      success     : function() {
+        gotoNext();
+      }
+    });
+  }
+
+  $( ".gotoNext" ).click(function() {
+    gotoNext();
+  });
 });
