d41b7b4b7efa3ef4b305d569f33c625a85e68924
Peter Jensen <peter.privalinec@gmail.com>
2017-01-24 15:31:48 +0100

    Fixed error page

--- static/js/sources/page_error.js
+++ static/js/sources/page_error.js
@@ -1,26 +1,36 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  // Add the missing url to the text
-  $("#url").text(window.location.pathname);
+    // Add the missing url to the text
+    var param = getParameterByName('p');
+    if (param) {
+        $("#url").text(param);
+    }
 
-  var raf = requestAnimationFrame || mozRequestAnimationFrame ||
-    webkitRequestAnimationFrame || msRequestAnimationFrame;
-  if (raf) {
-    raf(function() {
-      $("img.lazy").lazyload({
-        threshold : 500
-      });
-    });
-  }
+    var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+        webkitRequestAnimationFrame || msRequestAnimationFrame;
+    if (raf) {
+        raf(function () {
+            $("img.lazy").lazyload({
+                threshold: 500
+            });
+        });
+    }
 
-  // Fix the css locations dynamically
-  $("head link").each(function() {
-    for (var i=0; i<window.location.pathname.split(/\//g).length-2; i++)
-      $(this).attr("href", "../" + $(this).attr("href"));
-  });
+    function getParameterByName(name, url) {
+        if (!url) {
+            url = window.location.href;
+        }
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
 
-  var $error = $('#error'),
-    h = window.innerHeight - 280;
-  // height fixer
-  $error.css('min-height', h + 'px');
-})
+
+    var $error = $('#error'),
+        h = window.innerHeight - 280;
+    // height fixer
+    $error.css('min-height', h + 'px');
+});
