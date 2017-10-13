44322073b6405975380b628585fe62c18fc1a785
Peter Jensen <tuximail@gmail.com>
2017-10-13 14:55:09 +0200

    removed url result in error page

--- static/js/sources/page_error.js
+++ static/js/sources/page_error.js
@@ -1,24 +1,4 @@
 $(document).ready(function () {
-
-    // Add the missing url to the text
-    var param = getParameterByName('p');
-    if (param) {
-        $("#url").text(param);
-    }
-
-    function getParameterByName(name, url) {
-        if (!url) {
-            url = window.location.href;
-        }
-        name = name.replace(/[\[\]]/g, "\\$&");
-        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-            results = regex.exec(url);
-        if (!results) return null;
-        if (!results[2]) return '';
-        return decodeURIComponent(results[2].replace(/\+/g, " "));
-    }
-
-
     var $error = $('#error'),
         h = window.innerHeight - 280;
     // height fixer
